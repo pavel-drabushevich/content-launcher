25380e79c58a4a4be866ef41cc65b27cbb2a2ed7
Pavel Drobushevich <p.drobushevich@gmail.com>
2016-12-17 10:29:42 +0300

    Categories

--- app/js/index.js
+++ app/js/index.js
@@ -5,3 +5,15 @@ var remote = require('remote');
 var Tray = remote.require('tray');
 var Menu = remote.require('menu');
 var path = require('path');
+
+var closeEl = document.querySelector('.close');
+closeEl.addEventListener('click', function () {
+    ipc.send('close-main-window');
+});
+
+var categories = document.querySelector('.i-role-categories');
+var categoryEl = document.querySelector('.i-role-category');
+var categoryContent = document.querySelector('.i-role-category-content');
+categoryEl.addEventListener('click', function (e) {
+  categories.style.display = "none";
+});
