6f979c6c1e3d9a620c488b1dc860af2568cd1919
Pavel Drobushevich <p.drobushevich@gmail.com>
2016-12-17 11:00:17 +0300

    Back to categories

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,34 @@
 'use strict';
 
+var $ = require('jquery');
 var ipc = require('ipc');
 var remote = require('remote');
 var Tray = remote.require('tray');
 var Menu = remote.require('menu');
 var path = require('path');
 
-var closeEl = document.querySelector('.close');
-closeEl.addEventListener('click', function () {
+$('.close').on('click', function () {
     ipc.send('close-main-window');
 });
 
-var categories = document.querySelector('.i-role-categories');
-var categoryEl = document.querySelector('.i-role-category');
-var categoryContent = document.querySelector('.i-role-category-content');
-categoryEl.addEventListener('click', function (e) {
-  categories.style.display = "none";
+var $backButton = $('.i-role-back');
+var $categories = $('.i-role-categories');
+var $category = $('.i-role-category');
+var $categoryContent = $('.i-role-category-content');
+
+var showCategory = function(category) {
+  $categoryContent.append('<ul><li>Урок 1</li><li>Урок 2</li></ul>');
+  $categoryContent.show();
+}
+
+$category.on('click', function (e) {
+  $categories.hide();
+  $backButton.show();
+  showCategory('test');
+});
+
+$backButton.on('click', function (e) {
+  $backButton.hide();
+  $categoryContent.hide();
+  $categories.show();
 });
